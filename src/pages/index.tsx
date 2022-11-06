647efce3188bd5145224c9ab5394eb9372f2e6fb
jejemerong <9708jjw@gmail.com>
2022-11-06 18:47:09 +0900

    change title to Challenge Note

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,8 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
-        <meta
-          name="description"
-          content="Solana Scaffold"
-        />
+        <title>Challenge Note</title>
+        <meta name="description" content="Solana Scaffold" />
       </Head>
       <HomeView />
     </div>
